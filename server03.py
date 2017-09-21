26725c227d731643e50024388dbf63d320d7ff22
liyongchun168 <liyongchun168@yeah.net>
2017-09-22 07:25:46 +0800

    20170922

--- server03.py
+++ server03.py
@@ -34,6 +34,17 @@ class case_directory_index_file(object):
     def act(self,handler):
         handler.handle_file(self.index_path(handler))
         
+class case_directory_no_index_file(object):
+    '''Serve listing  a directory without index.html'''
+
+    def index_path(self,handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self,handler):
+        return os.path.isdir(handler.full_path) and not os.path.isfile(self.index_path(handler))
+    def act(self,handler):
+        handler.list_dir(handler.full_path)
+
 class case_always_fail(object):
     '''Base case if nothing else worked'''
 
@@ -52,11 +63,19 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
              case_existing_file(),
              case_directory_index_file(),
              case_always_fail()]
-
+              
+    Error_Page = """\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
     #Handle a GET request.
     def do_GET(self):
         try:
-            full_path = os.getcwd() + self.path
+            self.full_path = os.getcwd() + self.path
 
 #            if not os.path.exists(full_path):
 #                raise ServerEyxception("'{0}' not found".format(self.path))
@@ -67,9 +86,9 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 #            else:
 #                raise ServerException("Unknown object '{0}' ".format(self.path))
             for case in self.Cases:
-                handler = case()
-                if handler.test(self):
-                    handler.act(self)
+#                handler = case()
+                if case.test(self):
+                    case.act(self)
                     break
         except Exception as msg:
             self.handle_error(msg)
@@ -90,6 +109,26 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         </body>
         </html>
         """
+
+    Listing_Page = """\
+        <html>
+        <body>
+        <ul>{0}</ul>
+        </body>
+        </html>
+        """
+
+    def list_dir(self,full_path):
+        try:
+            entries = os.listdir(full_path)
+            bullets = ['<li>{0}</li>'.format(e) 
+                for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            self.send_content(page)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path,msg)
+            self.handle_error(msg)
+
     def handle_error(self,msg):
         content = self.Error_Page.format(path=self.path,msg=msg)
         self.send_content(content,404)
